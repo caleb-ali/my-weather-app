6b04f02277203aa2f807a23ccb8e8da73c68c8d0
caleb-ali <slimkally@gmail.com>
2023-01-08 23:07:03 +0100

    def location

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 // import axios
-import axios from 'axios';
+import axios from "axios";
 
 // import icons
 import {
@@ -11,7 +11,7 @@ import {
   IoMdSnow,
   IoMdThunderstorm,
   IoMdSearch,
-} from 'react-icons/io';
+} from "react-icons/io";
 
 import {
   BsCloudHaze2Fill,
@@ -20,21 +20,21 @@ import {
   BsWater,
   BsThermometer,
   BsWind,
-} from 'react-icons/bs';
+} from "react-icons/bs";
 
-import { TbTemperatureCelsius } from 'react-icons/tb';
-import { ImSpinner8 } from 'react-icons/im';
+import { TbTemperatureCelsius } from "react-icons/tb";
+import { ImSpinner9 } from "react-icons/im";
 
 // api key
-const APIkey = 'bcf2048bc3be154bded8f277f580ba2e';
+const APIkey = "bcf2048bc3be154bded8f277f580ba2e";
 
 const App = () => {
   const [data, setData] = useState(null);
-  const [location, setLocation] = useState('Bucharest');
-  const [inputValue, setInputValue] = useState('');
+  const [location, setLocation] = useState("Lagos");
+  const [inputValue, setInputValue] = useState("");
   const [animate, setAnimate] = useState(false);
   const [loading, setLoading] = useState(false);
-  const [errorMsg, setErrorMsg] = useState('');
+  const [errorMsg, setErrorMsg] = useState("");
 
   const handleInput = (e) => {
     setInputValue(e.target.value);
@@ -42,16 +42,16 @@ const App = () => {
 
   const handleSubmit = (e) => {
     // if input value is not empty
-    if (inputValue !== '') {
+    if (inputValue !== "") {
       // set location
       setLocation(inputValue);
     }
 
     // select input
-    const input = document.querySelector('input');
+    const input = document.querySelector("input");
 
     // if input value is empty
-    if (input.value === '') {
+    if (input.value === "") {
       // set animate to true
       setAnimate(true);
       // after 500 ms set animate to false
@@ -61,7 +61,7 @@ const App = () => {
     }
 
     // clear input
-    input.value = '';
+    input.value = "";
 
     // prevent defaults
     e.preventDefault();
@@ -93,7 +93,7 @@ const App = () => {
   // error message
   useEffect(() => {
     const timer = setTimeout(() => {
-      setErrorMsg('');
+      setErrorMsg("");
     }, 2000);
     // clear timer
     return () => clearTimeout(timer);
@@ -102,9 +102,9 @@ const App = () => {
   // if data is false show the loader
   if (!data) {
     return (
-      <div className='w-full h-screen bg-gradientBg bg-no-repeat bg-cover bg-center flex flex-col justify-center items-center'>
+      <div className="w-full h-screen bg-gradientBg bg-no-repeat bg-cover bg-center flex flex-col justify-center items-center">
         <div>
-          <ImSpinner8 className='text-5xl animate-spin text-white' />
+          <ImSpinner9 className="text-5xl animate-spin text-white" />
         </div>
       </div>
     );
@@ -114,25 +114,25 @@ const App = () => {
   let icon;
 
   switch (data.weather[0].main) {
-    case 'Clouds':
+    case "Clouds":
       icon = <IoMdCloudy />;
       break;
-    case 'Haze':
+    case "Haze":
       icon = <BsCloudHaze2Fill />;
       break;
-    case 'Rain':
-      icon = <IoMdRainy className='text-[#31cafb]' />;
+    case "Rain":
+      icon = <IoMdRainy className="text-[#31cafb]" />;
       break;
-    case 'Clear':
-      icon = <IoMdSunny className='text-[#ffde33]' />;
+    case "Clear":
+      icon = <IoMdSunny className="text-[#ffde33]" />;
       break;
-    case 'Drizzle':
-      icon = <BsCloudDrizzleFill className='text-[#31cafb]' />;
+    case "Drizzle":
+      icon = <BsCloudDrizzleFill className="text-[#31cafb]" />;
       break;
-    case 'Snow':
-      icon = <IoMdSnow className='text-[#31cafb]' />;
+    case "Snow":
+      icon = <IoMdSnow className="text-[#31cafb]" />;
       break;
-    case 'Thunderstorm':
+    case "Thunderstorm":
       icon = <IoMdThunderstorm />;
       break;
   }
@@ -141,47 +141,47 @@ const App = () => {
   const date = new Date();
 
   return (
-    <div className='w-full h-screen bg-gradientBg bg-no-repeat bg-cover bg-center flex flex-col items-center justify-center px-4 lg:px-0'>
+    <div className="w-full h-screen bg-gradientBg bg-no-repeat bg-cover bg-center flex flex-col items-center justify-center px-4 lg:px-0">
       {errorMsg && (
-        <div className='w-full max-w-[90vw] lg:max-w-[450px] bg-[#ff208c] text-white absolute top-2 lg:top-10 p-4 capitalize rounded-md'>{`${errorMsg.response.data.message}`}</div>
+        <div className="w-full max-w-[90vw] lg:max-w-[450px] bg-[#ff208c] text-white absolute top-2 lg:top-10 p-4 capitalize rounded-md">{`${errorMsg.response.data.message}`}</div>
       )}
       {/* form */}
       <form
         className={`${
-          animate ? 'animate-shake' : 'animate-none'
+          animate ? "animate-shake" : "animate-none"
         } h-16 bg-black/30 w-full max-w-[450px]
       rounded-full backdrop-blur-[32px] mb-8`}
       >
-        <div className='h-full relative flex items-center justify-between p-2'>
+        <div className="h-full relative flex items-center justify-between p-2">
           <input
             onChange={(e) => handleInput(e)}
-            className='flex-1 bg-transparent outline-none placeholder:text-white text-white text-[15px] font-light pl-6 h-full'
-            type='text'
-            placeholder='Search by city or country'
+            className="flex-1 bg-transparent outline-none placeholder:text-white text-white text-[15px] font-light pl-6 h-full"
+            type="text"
+            placeholder="Search by city or country"
           />
           <button
             onClick={(e) => handleSubmit(e)}
-            className='bg-[#1ab8ed] hover:bg-[#15abdd] w-20 h-12 rounded-full flex justify-center items-center transition'
+            className="bg-[#1ab8ed] hover:bg-[#15abdd] w-20 h-12 rounded-full flex justify-center items-center transition"
           >
-            <IoMdSearch className='text-2xl text-white' />
+            <IoMdSearch className="text-2xl text-white" />
           </button>
         </div>
       </form>
       {/* card */}
-      <div className='w-full max-w-[450px] bg-black/20 min-h-[584px] text-white backdrop-blur-[32px] rounded-[32px] py-12 px-6'>
+      <div className="w-full max-w-[450px] bg-black/20 min-h-[584px] text-white backdrop-blur-[32px] rounded-[32px] py-12 px-6">
         {loading ? (
-          <div className='w-full h-full flex justify-center items-center'>
-            <ImSpinner8 className='text-white text-5xl animate-spin' />
+          <div className="w-full h-full flex justify-center items-center">
+            <ImSpinner9 className="text-white text-5xl animate-spin" />
           </div>
         ) : (
           <div>
             {/* card top */}
-            <div className='flex items-center gap-x-5'>
+            <div className="flex items-center gap-x-5">
               {/* icon */}
-              <div className='text-[87px]'>{icon}</div>
+              <div className="text-[87px]">{icon}</div>
               <div>
                 {/* country name */}
-                <div className='text-2xl font-semibold'>
+                <div className="text-2xl font-semibold">
                   {data.name}, {data.sys.country}
                 </div>
                 {/* date */}
@@ -192,67 +192,67 @@ const App = () => {
               </div>
             </div>
             {/* card body */}
-            <div className='my-20'>
-              <div className='flex justify-center items-center'>
+            <div className="my-20">
+              <div className="flex justify-center items-center">
                 {/* temp */}
-                <div className='text-[144px] leading-none font-light'>
+                <div className="text-[144px] leading-none font-light">
                   {parseInt(data.main.temp)}
                 </div>
                 {/* celsius icon */}
-                <div className='text-4xl'>
+                <div className="text-4xl">
                   <TbTemperatureCelsius />
                 </div>
               </div>
               {/* weather description */}
-              <div className='capitalize text-center'>
+              <div className="capitalize text-center">
                 {data.weather[0].description}
               </div>
             </div>
             {/* card bottom */}
-            <div className='max-w-[378px] mx-auto flex flex-col gap-y-6'>
-              <div className='flex justify-between'>
-                <div className='flex items-center gap-x-2'>
+            <div className="max-w-[378px] mx-auto flex flex-col gap-y-6">
+              <div className="flex justify-between">
+                <div className="flex items-center gap-x-2">
                   {/* icon */}
-                  <div className='text-[20px]'>
+                  <div className="text-[20px]">
                     <BsEye />
                   </div>
                   <div>
-                    Visibility{' '}
-                    <span className='ml-2'>{data.visibility / 1000} km</span>
+                    Visibility{" "}
+                    <span className="ml-2">{data.visibility / 1000} km</span>
                   </div>
                 </div>
-                <div className='flex items-center gap-x-2'>
+                <div className="flex items-center gap-x-2">
                   {/* icon */}
-                  <div className='text-[20px]'>
+                  <div className="text-[20px]">
                     <BsThermometer />
                   </div>
-                  <div className='flex'>
+                  <div className="flex">
                     Feels like
-                    <div className='flex ml-2'>
+                    <div className="flex ml-2">
                       {parseInt(data.main.feels_like)}
                       <TbTemperatureCelsius />
                     </div>
                   </div>
                 </div>
               </div>
-              <div className='flex justify-between'>
-                <div className='flex items-center gap-x-2'>
+              <div className="flex justify-between">
+                <div className="flex items-center gap-x-2">
                   {/* icon */}
-                  <div className='text-[20px]'>
+                  <div className="text-[20px]">
                     <BsWater />
                   </div>
                   <div>
                     Humidity
-                    <span className='ml-2'>{data.main.humidity} %</span>
+                    <span className="ml-2">{data.main.humidity} %</span>
                   </div>
                 </div>
-                <div className='flex items-center gap-x-2'>
+                <div className="flex items-center gap-x-2">
                   {/* icon */}
-                  <div className='text-[20px]'>
+                  <div className="text-[20px]">
                     <BsWind />
                   </div>
                   <div>
-                    Wind <span className='ml-2'>{data.wind.speed} m/s</span>
+                    Wind <span className="ml-2">{data.wind.speed} m/s</span>
                   </div>
                 </div>
               </div>
